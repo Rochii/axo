eec1121cfe2262f5ee2feb88fab12755d41e5609
Jason Harris <sirmanlypowers@gmail.com>
2018-06-28 08:45:30 -0700

    TTP229: Added an object with boolean outputs.

--- work/objects/touch/ttp229.h
+++ work/objects/touch/ttp229.h
@@ -7,12 +7,17 @@ Author: Jason Harris (https://github.com/deadsy)
 http://www.tontek.com.tw/uploads/product/106/TTP229-LSF_V1.0_EN.pdf
 https://robotdyn.com/16-keys-capacitive-touch-ttp229-i2c-module.html
 
-Note 1: This object requires a single instance of the factory/gpio/i2c/config object.
-This allows multiple i2c devices (each with a unique i2c address) to work concurrently.
-I2C Bus: I2C1, SCL=PB8, SDA=PB9 (these are the config defaults)
+Notes:
 
-Note 2: There are other chips also called ttp229-x which do not use I2C.
+1: This object requires a single instance of the factory/gpio/i2c/config object.
+This allows multiple i2c devices (each with a unique i2c address) to work concurrently.
+I2C1 Bus, SCL=PB8, SDA=PB9 (these are the config defaults)
+
+2: There are other chips also called ttp229-x which do not use I2C.
 This driver supports the I2C variant. ie- TTP229-LSF
+
+3: The device only supports a single I2C address, so there can only be a single
+device on a given i2c bus.
 
 */
 //-----------------------------------------------------------------------------
@@ -22,9 +27,8 @@ This driver supports the I2C variant. ie- TTP229-LSF
 
 //-----------------------------------------------------------------------------
 
-// Only 1 supported i2c address :-(
-#define TTP229_I2C_ADR 0x57
-
+#define TTP229_I2C_ADR 0x57	// Only 1 supported i2c address :-(
+#define TTP229_PERIOD 32	// polling period in ms (8Hz)
 #define TTP229_I2C_TIMEOUT 30	// chibios ticks
 
 //-----------------------------------------------------------------------------
@@ -61,7 +65,7 @@ static void *ttp229_malloc(size_t size) {
 }
 
 //-----------------------------------------------------------------------------
-// i2c read routine
+// i2c read routine (the device is read only)
 
 // read a 16 bit value
 static int ttp229_rd16(struct ttp229_state *s, uint16_t * val) {
@@ -91,7 +95,7 @@ static msg_t ttp229_thread(void *arg) {
 	int rc = 0;
 	uint16_t val;
 
-	ttp229_info(s, "starting thread");
+	//ttp229_info(s, "starting thread");
 
 	// allocate i2c rx buffer
 	s->rx = (uint8_t *) ttp229_malloc(2);
@@ -109,15 +113,14 @@ static msg_t ttp229_thread(void *arg) {
 	while (!chThdShouldTerminate()) {
 		// read the touch status
 		ttp229_rd16(s, &val);
-		val &= 0xffff;
 		chSysLock();
 		s->touch = val;
 		chSysUnlock();
-		chThdSleepMilliseconds(16);	// ~64 Hz TODO- check
+		chThdSleepMilliseconds(32);	// 8 Hz per pin configuration
 	}
 
  exit:
-	ttp229_info(s, "stopping thread");
+	//ttp229_info(s, "stopping thread");
 	chThdExit((msg_t) 0);
 }
 
@@ -138,11 +141,43 @@ static void ttp229_dispose(struct ttp229_state *s) {
 	chThdWait(s->thd);
 }
 
-static void ttp229_krate_int(struct ttp229_state *s, int32_t * touch) {
+// krate with integer output
+static void ttp229_krate_int(struct ttp229_state *s,	//
+			     int32_t * touch	//
+    ) {
 	// get the current touch status
 	chSysLock();
 	*touch = s->touch;
 	chSysUnlock();
+}
+
+// krate with boolean output
+static void ttp229_krate_bool(struct ttp229_state *s,	//
+			      bool * o0, bool * o1, bool * o2, bool * o3,	//
+			      bool * o4, bool * o5, bool * o6, bool * o7,	//
+			      bool * o8, bool * o9, bool * o10, bool * o11,	//
+			      bool * o12, bool * o13, bool * o14, bool * o15	//
+    ) {
+	// get the current touch status
+	chSysLock();
+	int32_t touch = s->touch;
+	chSysUnlock();
+	*o0 = (touch >> 0) & 1;
+	*o1 = (touch >> 1) & 1;
+	*o2 = (touch >> 2) & 1;
+	*o3 = (touch >> 3) & 1;
+	*o4 = (touch >> 4) & 1;
+	*o5 = (touch >> 5) & 1;
+	*o6 = (touch >> 6) & 1;
+	*o7 = (touch >> 7) & 1;
+	*o8 = (touch >> 8) & 1;
+	*o9 = (touch >> 9) & 1;
+	*o10 = (touch >> 10) & 1;
+	*o11 = (touch >> 11) & 1;
+	*o12 = (touch >> 12) & 1;
+	*o13 = (touch >> 13) & 1;
+	*o14 = (touch >> 14) & 1;
+	*o15 = (touch >> 15) & 1;
 }
 
 //-----------------------------------------------------------------------------
